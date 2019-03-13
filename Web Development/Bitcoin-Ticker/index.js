02b467978fa1b023f203084ba4259aa36e04aea9
samvaio <samvaiotmp@gmail.com>
2019-03-13 00:30:29 -0500

    Bitcoin-Ticker Final

--- Web Development/Bitcoin-Ticker/index.js
+++ Web Development/Bitcoin-Ticker/index.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-const url = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD";
+
 
 const app = express();
 
@@ -14,8 +14,26 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  request(url, function(error, response, body) {
-    console.log(response.statusCode);
+  const crypto = req.body.crypto;
+  const fiat = req.body.fiat;
+  const amount = req.body.amount;
+
+  var options = {
+    url: "https://apiv2.bitcoinaverage.com/convert/global",
+    method: "GET",
+    qs: {
+      from: crypto,
+      to: fiat,
+      amount: amount
+    }
+  };
+  request(options, function(error, response, body) {
+    var data = JSON.parse(body);
+    var price = data.price;
+    var currentDate = data.time;
+    res.write("<p>The current date is " + currentDate+"</p>");
+    res.write("<h1>The price of "+ amount + " " + crypto + " is currently worth " + price + " in " +fiat+"</h1>");
+    res.send();
   });
 });
 

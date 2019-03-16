9cbffd905ed3820541d4d743efd94621c5840dae
samvaio <samvaiotmp@gmail.com>
2019-03-15 22:53:45 -0500

    Newsletter-Signup Final

--- Web Development/Newsletter-Signup/app.js
+++ Web Development/Newsletter-Signup/app.js
@@ -8,8 +8,68 @@ const request = require("request");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.listen(3000, function() {
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post("/", function(req, res) {
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us20.api.mailchimp.com/3.0/lists/6cb4ed96d0",
+    method: "POST",
+    headers: {
+      "Authorization": "anystring 62573887e937291e1c7be62707287f53-us20"
+    },
+    body: jsonData
+  };
+  request(options, function(error, response, body) {
+    if (error) {
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+  });
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on port 3000");
 });
+
+
+// 62573887e937291e1c7be62707287f53-us20
+
+// 6cb4ed96d0

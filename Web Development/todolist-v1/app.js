7d1aff2c11c2944fc4b8c3f1a299026a0b3455b9
Warner Gears <warnergears@warners-mini.lan>
2019-03-27 22:05:38 -0500

    todolist V1 final

--- Web Development/todolist-v1/app.js
+++ Web Development/todolist-v1/app.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const date = require(__dirname + "/date.js");
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
 app.set("view engine", "ejs");
 
 
@@ -17,22 +19,34 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-  res.render("list", {kindOfDay: day, newListItems: items});
+  let day = date.getDate();
+  res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
 });
 
 app.listen(process.env.PORT || 3000, function() {
